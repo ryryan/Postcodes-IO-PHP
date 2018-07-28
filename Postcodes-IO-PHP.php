5a5a6bfe9a640dc5ce943c15a6a0b5c0447a6bea
Ryan <ryhart18@gmail.com>
2018-07-28 20:30:53 +0100

    Better error handling
    Using cURL instead of file_get_contents.

--- Postcodes-IO-PHP.php
+++ Postcodes-IO-PHP.php
@@ -1,19 +1,23 @@
 <?php
-	//Put together by Ryan Hart 2016  www.ryhart.com
+	//Put together by Ryan Hart 2016
 	//Class to use the API provided by http://postcodes.io
 	
 	class Postcode{
 		
 		public function lookup($postcode){
 			$jsonurl = "https://api.postcodes.io/postcodes/".$postcode;
-			$json = file_get_contents($jsonurl);
-			
+			$json = $this->request($jsonurl);
 			$decoded = json_decode($json);
-			return $decoded->result;
+			if($decoded->status == 200){
+				return $decoded->result;
+			}
+			else{
+				return false;
+			}
+			return false;
 		}
 		public function bulkLookup($postcodes){
 			$data_string = json_encode(array('postcodes' => $postcodes));
-			
 			$ch = curl_init('https://api.postcodes.io/postcodes');
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
@@ -24,17 +28,28 @@
 			);
 			
 			$result = curl_exec($ch);
-			$result = json_decode($result);
-			
-			return $result->result;
-			
+			curl_close($ch);
+			$decoded = json_decode($result);
+			if($decoded->status == 200){
+				return $decoded->result;
+			}
+			else{
+				return false;
+			}
+			return false;
 		}
 		public function nearestPostcodesFromLongLat($longitude, $latitude){
 			$jsonurl = "https://api.postcodes.io/postcodes?lon=".$longitude."&lat=".$latitude;
-			$json = file_get_contents($jsonurl);
+			$json = $this->request($jsonurl);
 			
 			$decoded = json_decode($json);
-			return $decoded->result;
+			if($decoded->status == 200){
+				return $decoded->result;
+			}
+			else{
+				return false;
+			}
+			return false;
 		}
 		public function bulkReverseGeocoding($geolocations){
 			$data_string = json_encode(array('geolocations' => $geolocations));
@@ -49,26 +64,41 @@
 			);
 			
 			$result = curl_exec($ch);
-			$result = json_decode($result);
-			
-			return $result->result;
-			
+			curl_close($ch);
+			$decoded = json_decode($result);
+			if($decoded->status == 200){
+				return $decoded->result;
+			}
+			else{
+				return false;
+			}
+			return false;
 		}
 		public function random(){
 			$jsonurl = "https://api.postcodes.io/random/postcodes/";
-			$json = file_get_contents($jsonurl);
+			$json = $this->request($jsonurl);
 			
 			$decoded = json_decode($json);
-			return $decoded->result;
+			if($decoded->status == 200){
+				return $decoded->result;
+			}
+			else{
+				return false;
+			}
+			return false;
 		}
 		public function validate($postcode){
 			$jsonurl = "https://api.postcodes.io/postcodes/".$postcode."/validate";
-			$json = file_get_contents($jsonurl);
+			$json = $this->request($jsonurl);
 			
 			$decoded = json_decode($json);
-			
-			if($decoded->result == 1){
-				return true;	
+			if($decoded->status == 200){
+				if($decoded->result == 1){
+					return true;	
+				}
+				else{
+					return false;
+				}
 			}
 			else{
 				return false;
@@ -77,45 +107,94 @@
 		}
 		public function nearest($postcode){
 			$jsonurl = "https://api.postcodes.io/postcodes/".$postcode."/nearest";
-			$json = file_get_contents($jsonurl);
+			$json = $this->request($jsonurl);
 			
 			$decoded = json_decode($json);
-			return $decoded->result;
+			if($decoded->status == 200){
+				return $decoded->result;
+			}
+			else{
+				return false;
+			}
+			return false;
 		}
 		public function partial($postcode){
 			$jsonurl = "https://api.postcodes.io/postcodes/".$postcode."/autocomplete";
-			$json = file_get_contents($jsonurl);
+			$json = $this->request($jsonurl);
 			
 			$decoded = json_decode($json);
-			return $decoded->result;
+			if($decoded->status == 200){
+				return $decoded->result;
+			}
+			else{
+				return false;
+			}
+			return false;
 		}
 		public function query($postcode){
 			$jsonurl = "https://api.postcodes.io/postcodes?q=".$postcode;
-			$json = file_get_contents($jsonurl);
+			$json = $this->request($jsonurl);
 			
 			$decoded = json_decode($json);
-			return $decoded->result;
+			if($decoded->status == 200){
+				return $decoded->result;
+			}
+			else{
+				return false;
+			}
+			return false;
+		}
+		public function lookupTerminated($postcode){
+			$jsonurl = "https://api.postcodes.io/terminated_postcodes/".$postcode;
+			$json = $this->request($jsonurl);
+			
+			$decoded = json_decode($json);
+			if($decoded->status == 200){
+				return $decoded->result;
+			}
+			else{
+				return false;
+			}
+			return false;
 		}
 		public function lookupOutwardCode($code){
 			$jsonurl = "https://api.postcodes.io/outcodes/".$code;
-			$json = file_get_contents($jsonurl);
+			$json = $this->request($jsonurl);
 			
 			$decoded = json_decode($json);
-			return $decoded->result;
+			if($decoded->status == 200){
+				return $decoded->result;
+			}
+			else{
+				return false;
+			}
+			return false;
 		}
 		public function nearestOutwardCode($code){
 			$jsonurl = "https://api.postcodes.io/outcodes/".$code."/nearest";
-			$json = file_get_contents($jsonurl);
+			$json = $this->request($jsonurl);
 			
 			$decoded = json_decode($json);
-			return $decoded->result;
+			if($decoded->status == 200){
+				return $decoded->result;
+			}
+			else{
+				return false;
+			}
+			return false;
 		}
 		public function nearestOutwardCodeFromLongLat($longitude, $latitude){
 			$jsonurl = "https://api.postcodes.io/outcodes?lon=".$longitude."&lat=".$latitude;
-			$json = file_get_contents($jsonurl);
+			$json = $this->request($jsonurl);
 			
 			$decoded = json_decode($json);
-			return $decoded->result;
+			if($decoded->status == 200){
+				return $decoded->result;
+			}
+			else{
+				return false;
+			}
+			return false;
 		}
 		public function distance($postcode1, $postcode2, $unit){
 			//adapted from http://www.geodatasource.com/developers/php
@@ -146,6 +225,18 @@
 			} else {
 			    return $miles;
 			}	
+		}
+		public function request($jsonurl){
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, str_replace(' ', '%20', $jsonurl));
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+			curl_setopt($ch, CURLOPT_HTTPHEADER, [
+			  'Content-Type: application/json',
+			]);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			$response = curl_exec($ch);
+			curl_close($ch);
+			return $response;
 		}
 	}
 ?>
